a86a5ace9ac732f098743d5847e835cd495a8b1d
Suhail Gupta <suhailgupta03@gmail.com>
2023-11-29 20:01:54 +0530

    Update index.js

--- src/logger/index.js
+++ src/logger/index.js
@@ -11,6 +11,7 @@ const logger = winston.createLogger({
       //
       new winston.transports.File({ filename: 'error.log', level: 'error' }),
       new winston.transports.File({ filename: 'combined.log' }),
+      new winston.transports.Console()
     ],
   });
 
